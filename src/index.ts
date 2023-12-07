978e2aef2bb9ba5f4cfb3030c6fe5f0d6bf258d7
vylsaz <cokelover7244@proton.me>
2023-12-07 00:09:55 -0800

    Happy now?

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,22 @@ import { IEditorLanguageRegistry } from '@jupyterlab/codemirror';
 import { bqn } from 'lang-bqn';
 
 /**
-* Initialization data for the jupyterlab-bqn extension.
-*/
+ * Initialization data for the jupyterlab-bqn extension.
+ */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-bqn:plugin',
   description: 'A JupyterLab extension for the BQN language.',
   autoStart: true,
   requires: [IEditorLanguageRegistry],
   activate: (app: JupyterFrontEnd, language: IEditorLanguageRegistry) => {
-      const bqnSupport = bqn();
-      language.addLanguage({
-          name: 'bqn',
-          mime: 'text/bqn',
-          support: bqnSupport,
-          extensions: ['bqn'],
-      });
-      console.log('JupyterLab extension jupyterlab-bqn is activated!');
+    const bqnSupport = bqn();
+    language.addLanguage({
+      name: 'bqn',
+      mime: 'text/bqn',
+      support: bqnSupport,
+      extensions: ['bqn']
+    });
+    console.log('JupyterLab extension jupyterlab-bqn is activated!');
   }
 };
 
